164a13bc728de62a3c1808d07b79ca05711a45bb
David Brennan <davidjabrennan@gmail.com>
2019-12-03 07:29:04 +0000

    Day 3 Challenge 1

---
+++ day3/Day 3 Challenge 1.py
@@ -0,0 +1,44 @@
+import sys
+
+wires = [i.split(",") for i in open("input.txt").read().splitlines()]
+paths = dict()
+for index, wire in enumerate(wires):
+    point = [0, 0]
+    points = ["0,0"]
+    for movement in wire:
+        # print("Running wire: " + str(index) + " Step " + str(wire.index(movement)) + " of " + str(len(wire)))
+        direction = movement[0]
+        distance = int(movement[1::])
+        horizontal_distance = 0
+        vertical_distance = 0
+        if direction == 'L' or direction == 'R':
+            horizontal_distance = distance
+        elif direction == 'U' or direction == 'D':
+            vertical_distance = distance
+
+        for x in range(1, horizontal_distance + 1):
+            if direction == 'L':
+                point[0] = point[0] - 1
+            elif direction == 'R':
+                point[0] = point[0] + 1
+            points.append(str(point[0]) + "," + str(point[1]))
+
+        for y in range(1, vertical_distance + 1):
+            if direction == 'U':
+                point[1] = point[1] + 1
+            elif direction == 'D':
+                point[1] = point[1] - 1
+            points.append(str(point[0]) + "," + str(point[1]))
+    paths[index] = points
+# print(paths)
+intersections = set(paths[0]) & set(paths[1])
+intersections.remove("0,0")
+
+smallest_distance = sys.maxsize
+for intersection in intersections:
+    translated = [abs(int(x)) for x in intersection.split(",")]
+    manhatten_distance = sum(translated)
+    if manhatten_distance < smallest_distance:
+        smallest_distance = manhatten_distance
+print(intersections)
+print(smallest_distance)
